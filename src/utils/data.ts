df494bd60cbfe5221cc72a3d9cbb0a5e6174a1a0
Kauã Gabriel <kauagabrielramkg@gmail.com>
2024-02-16 10:20:11 -0300

    info update on segmentos

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,55 +1,25 @@
 const services = [
     {
-        title: "Chatbot",
-        description: "Solução de IA para inter-relação com clientes nos diversos canais, web, whatsapp, sms e e-mail.",
+        title: "Tiro esportivo",
+        description: "Na prática competitiva de tiro, os participantes demonstram precisão e habilidade ao disparar armas de fogo em direção a alvos, competindo em diversas categorias e modalidades, com foco na técnica e concentração.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "CRM Analytics",
-        description: "Análise das carteiras e evoluções, mapa de acompanhamento, BI e dashboard, gráficos evoluções e projeções.",
+        title: "Coleção militaria",
+        description: "A coleção militaria abrange objetos históricos e militares, como uniformes, armas, insígnias e equipamentos. Os colecionadores buscam preservar e estudar esses artefatos, proporcionando insights sobre períodos e eventos históricos.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Integrador",
-        description: "Conexão das soluções com plataformas, clientes e API, alertas de status e atividades, conexões com demais soluções e ajustes.",
+        title: "Caçadores e caça",
+        description: "Na modalidade de caça, os praticantes exploram ambientes naturais em busca de presas, utilizando armas específicas e técnicas de rastreamento. além do desafio físico, a caça proporciona uma conexão intima com a natureza e o ambiente selvagem.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Portal de cobranças",
-        description: "Soluções inteligentes para distribuição de carteiras, ativação, acompanhamento, segmentação, área logada e integração com o orquestrador.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Orquestrador",
-        description: "Solução de gestão dos canais e parâmetros de distribuição, inteligência temporal, análise de distribuição, redirecionamento, histórico de tráfego evolução e dashboard.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Remarketing",
-        description: "O remarketing em cobrança oferece uma segunda chance estratégica, reengajando devedores e otimizando a recuperação financeira.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Funil de recuperação",
-        description: "O funil de recuperação é a trajetória estruturada que guia devedores, desde o primeiro contato até a plena reabilitação financeira, maximizando a eficiência na gestão de cobranças.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Dashboard de recuperação",
-        description: "O dashboard de recuperação oferece uma visão consolidada e em tempo real das métricas essenciais, capacitando gestores a tomar decisões informadas e acelerar o processo de recuperação financeira.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
-        title: "Analytics",
-        description: "Analytics na gestão de cobranças proporciona insights valiosos, permitindo análises profundas para aprimorar estratégias, identificar padrões e impulsionar a eficácia na recuperação de valores.",
+        title: "Cinematografia para eventos",
+        description: "Na consultoria de armas para cinematografia, especialistas orientam na seleção de armamentos apropriados para filmes, considerando a época e o contexto histórico da narrativa. Esse serviço contribui para a autenticidade e fidelidade visual das produções cinematográficas.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
