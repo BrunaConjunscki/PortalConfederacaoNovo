3d9440dc3b640c8a29f48c314a851b1bc8d09386
Kauã Gabriel <kauagabrielramkg@gmail.com>
2024-02-19 11:44:30 -0300

    background blurs update

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,22 +2,22 @@ const services = [
     {
         title: "Tiro esportivo",
         description: "Na prática competitiva de tiro, os participantes demonstram precisão e habilidade ao disparar armas de fogo em direção a alvos, competindo em diversas categorias e modalidades, com foco na técnica e concentração.",
-        icon: `<img src="./public/images/campo-de-tiro.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
+        icon: `<img src="./images/campo-de-tiro.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
     },
     {
         title: "Coleção militaria",
         description: "A coleção militaria abrange objetos históricos e militares, como uniformes, armas, insígnias e equipamentos. Os colecionadores buscam preservar e estudar esses artefatos, proporcionando insights sobre períodos e eventos históricos.",
-        icon: `<img src="./public/images/rifle.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
+        icon: `<img src="/images/rifle.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
     },
     {
         title: "Caçadores e caça",
         description: "Na modalidade de caça, os praticantes exploram ambientes naturais em busca de presas, utilizando armas específicas e técnicas de rastreamento. além do desafio físico, a caça proporciona uma conexão intima com a natureza e o ambiente selvagem.",
-        icon: `<img src="./public/images/cacando.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
+        icon: `<img src="/images/cacando.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
     },
     {
         title: "Cinematografia para eventos",
         description: "Na consultoria de armas para cinematografia, especialistas orientam na seleção de armamentos apropriados para filmes, considerando a época e o contexto histórico da narrativa. Esse serviço contribui para a autenticidade e fidelidade visual das produções cinematográficas.",
-        icon: `<img src="./public/images/filme-de-acao.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
+        icon: `<img src="/images/filme-de-acao.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
     },
 ];
 
