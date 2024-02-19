0925c67c46c62ad70e5fb276f9b6c09cb1d16f3e
Kauã Gabriel <kauagabrielramkg@gmail.com>
2024-02-19 10:59:57 -0300

    icons added

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,25 +2,22 @@ const services = [
     {
         title: "Tiro esportivo",
         description: "Na prática competitiva de tiro, os participantes demonstram precisão e habilidade ao disparar armas de fogo em direção a alvos, competindo em diversas categorias e modalidades, com foco na técnica e concentração.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" data-name="Layer 1" viewBox="0 0 24 24" stroke="#083597" id="target"><path fill="#fff" d="M12.5,19.975m2.169-.83a7.087,7.087,0,0,0,4.494-4.532A7.036,7.036,0,0,1,14.669,19.145Zm-4.359-.007a7.039,7.039,0,0,1-4.45-4.45A7.091,7.091,0,0,0,10.31,19.138Zm9.2-6.638h0Zm-5.014,0a2,2,0,0,0-4,0,2,2,0,0,1,4,0Zm-9.007,0h0Zm13.676-2.1A7.089,7.089,0,0,0,14.6,5.83,7.034,7.034,0,0,1,19.169,10.4ZM5.854,10.329a7.036,7.036,0,0,1,4.531-4.493A7.084,7.084,0,0,0,5.854,10.329ZM12.5,4.5v0Z"></path><path d="M12.5,4a.5.5,0,0,0-.5.5v.525A7.5,7.5,0,0,0,5.025,12H4.5a.5.5,0,0,0,0,1h.525A7.5,7.5,0,0,0,12,19.975V20.5a.5.5,0,0,0,1,0v-.525A7.5,7.5,0,0,0,19.975,13H20.5a.5.5,0,0,0,0-1h-.525A7.5,7.5,0,0,0,13,5.025V4.5a.5.5,0,0,0-.5-.5Zm0,3a.5.5,0,0,0,.5-.5V6.025A6.5,6.5,0,0,1,18.975,12H18.5a.5.5,0,0,0,0,1h.475A6.5,6.5,0,0,1,13,18.975V18.5a.5.5,0,0,0-1,0v.475A6.5,6.5,0,0,1,6.025,13H6.5a.5.5,0,0,0,0-1H6.025A6.5,6.5,0,0,1,12,6.025V6.5a.5.5,0,0,0,.5.5Zm0,3A2.5,2.5,0,1,0,15,12.5,2.5,2.5,0,0,0,12.5,10Zm0,4A1.5,1.5,0,1,1,14,12.5,1.5,1.5,0,0,1,12.5,14Z"></path></svg>`,
+        icon: `<img src="./public/images/campo-de-tiro.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
     },
     {
         title: "Coleção militaria",
         description: "A coleção militaria abrange objetos históricos e militares, como uniformes, armas, insígnias e equipamentos. Os colecionadores buscam preservar e estudar esses artefatos, proporcionando insights sobre períodos e eventos históricos.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffc436" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: `<img src="./public/images/rifle.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
     },
     {
         title: "Caçadores e caça",
         description: "Na modalidade de caça, os praticantes exploram ambientes naturais em busca de presas, utilizando armas específicas e técnicas de rastreamento. além do desafio físico, a caça proporciona uma conexão intima com a natureza e o ambiente selvagem.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: `<img src="./public/images/cacando.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
     },
     {
         title: "Cinematografia para eventos",
         description: "Na consultoria de armas para cinematografia, especialistas orientam na seleção de armamentos apropriados para filmes, considerando a época e o contexto histórico da narrativa. Esse serviço contribui para a autenticidade e fidelidade visual das produções cinematográficas.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: `<img src="./public/images/filme-de-acao.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
     },
 ];
 
