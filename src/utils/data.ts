beea53c8a86471054fa80bc87b7d616b860827b4
Bruna Conjunscki <brunaconjunsckideoliveira@hotmail.com>
2024-03-08 08:27:37 -0300

    alteracoes-texto

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -6,7 +6,7 @@ const services = [
     },
     {
         title: "Coleção militaria",
-        description: "Oferecemos serviços especializados para colecionadores militares, incluindo avaliação, inventário e conservação de peças históricas. Nossa orientação visa preservar e estudar artefatos como armas e identificação de munição, orientando em diversas coleções.",
+        description: "Oferecemos serviços especializados para colecionadores militares, incluindo avaliação, inventário e conservação de peças históricas. Nossa orientação em diversas coleções visa preservar e estudar artefatos como armas e identificação de munição.",
         icon: `<img src="/images/rifle.png" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#CD35C0" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>`,
         href: "https://www.flickr.com/photos/saltsystems/albums/72177720314898138",
     },
