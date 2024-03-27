6910c6de9c2a3bed2bab885329dddce10c1fe12b
Kauã Gabriel <kauagabrielramkg@gmail.com>
2024-03-27 09:23:12 -0300

    update port

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()], 
-  server: { port: 8080, host:"0.0.0.0"}
+  server: { port: 8082, host:"0.0.0.0"}
 });
